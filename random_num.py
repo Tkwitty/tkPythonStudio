f80dcf4c0cf1fd14f96da1ebab4a0c7d1fbba596
Tkwitty <35372237+Tkwitty@users.noreply.github.com>
2018-07-15 16:35:25 +0800

    this is my first time to commit my code - prt

--- random_num.py
+++ random_num.py
@@ -1,8 +1,10 @@
-# -*- coding: gbk -*-
+# -*- coding: utf8 -*-
 import random
 
-#2018.6.16-数字图像处理【助教】实验评分
+# 2018.6.16-数字图像处理【助教】实验评分
 
 for i in range(0, 60): # range左包含右不包含
     print(random.randint(12, 20))
     
+    
+print("to this tome, the random nums have been product")
